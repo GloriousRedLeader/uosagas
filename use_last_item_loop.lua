b8d01b88d798e27319d7bfa2dbe635844a0e9616
arturo the burro <arturotheburro7@gmail.com>
2026-04-06 12:41:59 -0700

    formatting

--- use_last_item_loop.lua
+++ use_last_item_loop.lua
@@ -4,16 +4,38 @@
 -- by OMG Arturo
 ------------------------------------------------------------------------------------
 
+-- Don't screw around with this.
+local VERSION = "1.3"
 
-local delay = 1000
+-- Probably don't mess with this either
+local ACTION_DELAY = 750
 
 ------------------------------------------------------------------------------------
--- END OPTIONS 
+-- END OPTIONS
 -- by OMG Arturo
 ------------------------------------------------------------------------------------
+
+-- Define Color Scheme
+local Colors = {
+    Alert   = 33,       -- Red
+    Warning = 48,       -- Orange
+    Caution = 53,       -- Yellow
+    Action  = 67,       -- Green
+    Confirm = 73,       -- Light Green
+    Info    = 84,       -- Light Blue
+    Status  = 93        -- Blue
+}
+
+-- Print Initial Start-Up Greeting
+Messages.Print("___________________________________", Colors.Info)
+Messages.Print("Double-clicker Online (v" .. VERSION .. ")", Colors.Info)
+Messages.Print("D-Clicks an object many times", Colors.Info)
+Messages.Print("__________________________________", Colors.Info)
+
+Messages.Print("Select an object", Colors.Confirm)
 
 obj = Targeting.GetNewTarget()
 while true do
     Player.UseObject(obj)
-    Pause(delay)
+    Pause(ACTION_DELAY)
 end
